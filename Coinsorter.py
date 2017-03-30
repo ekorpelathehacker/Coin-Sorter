253a30708224a4ff154308b983eac6ee1ac22691
Eino Korpela <ekorpela2020@pingry.org>
2017-03-30 12:36:57 -0400

    This devides the number of coins into different coins like quarters, nickles, etc..

--- Coinsorter.py
+++ Coinsorter.py
@@ -1,5 +1,13 @@
 coinamount = input ("Enter an amount of cents ")
 coinamount = int(coinamount)
+q = int(coinamount/25)
+coinamount = coinamount -(q * 25)
+d = int(coinamount/10)
+coinamount = coinamount - (d * 10)
+n = int(coinamount/5)
+coinamount = coinamount - (n * 5)
+p = int(coinamount/1)
+coinamount = coinamount - (p * 1)
 
 
 
