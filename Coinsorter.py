347804aabd7dd4b5aacef30649d926c167113bb7
Eino Korpela <ekorpela2020@pingry.org>
2017-03-30 12:32:18 -0400

    This asks for number of cents and makes it a int

---
+++ Coinsorter.py
@@ -0,0 +1,26 @@
+coinamount = input ("Enter an amount of cents ")
+coinamount = int(coinamount)
+
+
+
+
+
+
+
+
+
+#coinamount = input ("Enter an amount of cents ")
+#coinamount = int(coinamount)
+#q = int(coinamount/25)
+#coinamount = coinamount -(q * 25)
+#d = int(coinamount/10)
+#coinamount = coinamount - (d * 10)
+#n = int(coinamount/5)
+#coinamount = coinamount - (n * 5)
+#p = int(coinamount/1)
+#coinamount = coinamount - (p * 1)
+
+#output = str(coinamount) + "is {} quarters, {} dimes, {} nickels, and {} pennys
+#output = output.format (q, d ,n ,p)
+#print(output)
+
